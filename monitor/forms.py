0dc67fdaa24a40ab711a2d0b11596daa8f71da6c
kzmrt <info.murata.kazuhiko@gmail.com>
2018-12-18 16:44:12 +0900

    アップロード対象ファイルの拡張子バリデーション処理を追加。

--- monitor/forms.py
+++ monitor/forms.py
@@ -1,5 +1,16 @@
 from django import forms
+import os
+
+VALID_EXTENSIONS = ['.csv']
 
 
 class UploadFileForm(forms.Form):
-    file = forms.FileField()
+    file = forms.FileField(
+        label='アップロードファイル',
+    )
+
+    def clean_file(self):
+        file = self.cleaned_data['file']
+        extension = os.path.splitext(file.name)[1] # 拡張子を取得
+        if not extension.lower() in VALID_EXTENSIONS:
+            raise forms.ValidationError('csvファイルを選択してください！')
